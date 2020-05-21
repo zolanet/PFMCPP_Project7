272fccadef39d84b81b6b3c6a8ba16dbe99ab533
Charles Schiermeyer <matkatmusic@gmail.com>
2020-05-20 19:19:58 -0700

    Added part 1 instructions

---
+++ DragonSlayer.cpp
@@ -0,0 +1,28 @@
+#include "DragonSlayer.h"
+#include "Dragon.h"
+
+//DragonSlayer::DragonSlayer
+
+//DragonSlayer::getName
+
+void DragonSlayer::attack(Character& other)
+{
+    std::cout << name << " is attacking " << other.getName() << " !!" << std::endl;
+    if( auto* dragon = dynamic_cast<Dragon*>(&other) )
+    {
+        assert(false);
+        //DragonSlayers get a 10x boost when attacking dragons, from their attack item.
+        //so they should USE their attack item before attacking the dragon... 
+        //note that items are single-use only, so you need to reset it after use.  
+        //look in the Character class for how the other item types are reset after use.
+        while( dragon->getHP() > 0 )
+        {
+          dragon->takeDamage(attackDamage);
+        }
+    }
+        
+    Character::attack(other);
+        
+}
+
+//DragonSlayer::getStats
